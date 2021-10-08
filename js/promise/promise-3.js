9495513ddf9bebab330148df13adb086c535a082
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-10-09 01:15:49 +0300

    Update promise-3.js

--- js/promise/promise-3.js
+++ js/promise/promise-3.js
@@ -9,7 +9,7 @@ const randomIntegerFromInterval = (min, max) => {
 
 const makeTransaction = (transaction) => {
 
-    const promise = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         const delay = randomIntegerFromInterval(200, 500);
         setTimeout(() => {
             const canProcess = Math.random() > 0.3;
@@ -21,7 +21,6 @@ const makeTransaction = (transaction) => {
             }
         }, delay);
     })
-    return promise;
 };
 
 const logSuccess = ({ id, time }) => {
