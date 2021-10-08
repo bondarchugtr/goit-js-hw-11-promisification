4f4f88607dd9c476f79c074316341add4c9c44b8
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-10-09 01:05:40 +0300

    add promise - 3
    promise-3 +

--- js/promise/promise-3.js
+++ js/promise/promise-3.js
@@ -8,22 +8,21 @@ const randomIntegerFromInterval = (min, max) => {
 };
 
 const makeTransaction = (transaction) => {
-    const delay = randomIntegerFromInterval(200, 500);
     return new Promise((resolve, reject) => {
+        const delay = randomIntegerFromInterval(200, 500);
         setTimeout(() => {
             const canProcess = Math.random() > 0.3;
 
             if (canProcess) {
-                resolve(transaction.id, delay);
+                resolve({ id: transaction.id, time: delay });
             } else {
                 reject(transaction.id);
             }
         }, delay);
     })
-
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
     console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
